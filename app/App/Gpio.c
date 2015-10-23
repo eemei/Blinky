88e4f77800e9959c9051086d5cfae5a16f5a4874
eemei <lieweemei911@hotmail.com>
2015-10-23 15:28:55 +0800

    pg1314

--- app/App/Gpio.c
+++ app/App/Gpio.c
@@ -1,23 +1,43 @@
 #include "Gpio.h"
 #include "stm32f4xx_hal_rcc.h"
 
-
  void configurePort(int direction, int pinNum, GPIO_TypeDef *port) {
 
 	 GPIO_InitTypeDef GpioInfo;
+
 	 __HAL_RCC_GPIOG_CLK_ENABLE();
-	 GpioInfo.Pin = pinNum;
+	 __HAL_RCC_GPIOC_CLK_ENABLE();
+	 __HAL_RCC_GPIOB_CLK_ENABLE();
+
+	 int *ptr = (int *)0x40021800;
+	 ptr[0] = 0x14000000; 	//mode.pin14= output
+	 ptr[1] = 0; 			//otype.pin14 = push-pull;
+	 ptr[2] = 0x28000000;	//ospeed.pin14 = high speed;
+
+	 int *ptr1 = (int *)0x40020800;	//portc
+	 ptr1[0] = 0x10000000;
+	 ptr1[1] = 0;
+	 ptr1[2] = 0x20000000;
+
+	 int *ptr3 = (int *)0x40020400;	//portB
+	 ptr3[0] = 0x10000000;
+	 ptr3[1] = 0;
+	 ptr3[2] = 0x20000000;
+
+	/* GpioInfo.Pin = pinNum;
 	 GpioInfo.Mode = direction;
 	 GpioInfo.Speed = GPIO_SPEED_HIGH;
 	 GpioInfo.Pull = GPIO_NOPULL;
 	 HAL_GPIO_Init(port, &GpioInfo);
- }
+	*/
+
+ 	 }
 
 
-	void writeOne(int pinNum, GPIO_TypeDef *port) {
-		HAL_GPIO_WritePin(port, pinNum, GPIO_PIN_SET);
+void writeOne(int pinNum, GPIO_TypeDef *port) {
+	HAL_GPIO_WritePin(port, pinNum, GPIO_PIN_SET);
 	}
 
-	void writeZero (int pinNum, GPIO_TypeDef *port){
-		HAL_GPIO_WritePin(port,pinNum, GPIO_PIN_RESET);
+void writeZero (int pinNum, GPIO_TypeDef *port){
+	HAL_GPIO_WritePin(port,pinNum, GPIO_PIN_RESET);
 	}
--- app/main.c
+++ app/main.c
@@ -3,16 +3,32 @@
 #include "Time.h"
 #include "Gpio.h"
 
+void delay (uint32_t delaycount){
+	while (delaycount != 0){
+		delaycount--;
+	}
 
-int main(void)
-{
+}
+
+int main(void) {
+	configurePort(GPIO_OUTPUT, GPIO_PIN_14, GPIOG);
 	configurePort(GPIO_OUTPUT, GPIO_PIN_13, GPIOG);
+	configurePort(GPIO_OUTPUT, GPIO_PIN_13, GPIOB); //GREEN
+	configurePort(GPIO_OUTPUT, GPIO_PIN_5, GPIOC);	//RED
 
-    while(1)
-    {
-    	writeOne(GPIO_PIN_13, GPIOG);
+
+    while(1) {
+    	delay(1000000);
+    	writeOne(GPIO_PIN_14, GPIOG);
     	writeZero(GPIO_PIN_13, GPIOG);
+    	writeOne(GPIO_PIN_5, GPIOC);
+    	writeZero(GPIO_PIN_13, GPIOB);
 
-
+    	delay(1000000);
+    	writeZero(GPIO_PIN_14, GPIOG);
+    	writeOne(GPIO_PIN_13, GPIOG);
+    	writeZero(GPIO_PIN_5, GPIOC);
+    	writeOne(GPIO_PIN_13, GPIOB);
     }
+
 }
